5b73bc5f8161896c02478e4fb5b4432c24260485
Maksim Nemchenya <m.nemchenya@itransition.com>
2024-04-07 01:58:51 +0300

    Add project files.

---
+++ AGSR.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+string apiUrl = $"http://172.24.0.3:80/WeatherForecast";
+
+using (var httpClientHandler = new HttpClientHandler())
+{
+    httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+    {
+        return true;
+    };
+
+    using (var httpClient = new HttpClient(httpClientHandler))
+    {
+        var response = await httpClient.GetAsync(apiUrl);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+        }
+        else
+        {
+            Console.WriteLine("An error occurred: " + response.StatusCode);
+        }
+    }
+}
